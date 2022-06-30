6db0303ca61a10997733a47d46a9c370b7fe6380
nguyennp1 <nguyennp@its-global.vn>
2022-06-30 09:06:49 +0700

    remove all space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   protect_from_forgery with: :exception
- 
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
   protected
  
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: [:login,:password, :password_confirmation]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
